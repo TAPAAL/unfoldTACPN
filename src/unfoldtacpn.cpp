8a52e7021a5d46868c587f1e3403960cdef251e4
Peter Taankvist <ptaankvist@gmail.com>
2021-05-13 14:25:39 +0200

    Revert

--- src/unfoldtacpn.cpp
+++ src/unfoldtacpn.cpp
@@ -212,7 +212,58 @@ void writeQueries(vector<std::shared_ptr<Condition>>& queries, vector<std::strin
     out.close();
 }
 
-//void unfoldNet(ifstream& inputModelFile, ifstream& inputQueryFile, ostream& outputModelFile, fstream& outputQueryFile,unfoldtacpn_options_t options) {
+void unfoldNet(ifstream& inputModelFile, ifstream& inputQueryFile, ostream& outputModelFile, fstream& outputQueryFile,unfoldtacpn_options_t options) {
+    ColoredPetriNetBuilder cpnBuilder;
+    if(parseModel(cpnBuilder, inputModelFile) != ContinueCode) 
+    {
+        std::cerr << "Error parsing the model" << std::endl;
+        //return ErrorCode;
+    }
+    if (options.printstatistics) {
+        std::cout << "Finished parsing model" << std::endl;
+    }
     
-//}
+
+    //----------------------- Parse Query -----------------------//
+    std::vector<std::string> querynames;
+    auto queries = readQueries(options, querynames, inputQueryFile);
+
+    auto builder = cpnBuilder.unfold();
+    printUnfoldingStats(cpnBuilder, options);
+    builder.sort();
+    
+    //----------------------- Query Simplification -----------------------//
+    PetriNetBuilder b2(builder);
+    std::unique_ptr<PetriNet> qnet(b2.makePetriNet(false));
+
+    if(queries.size() == 0 || contextAnalysis(cpnBuilder, b2, qnet.get(), queries) != ContinueCode)
+    {
+        std::cerr << "Could not analyze the queries" << std::endl;
+        //return ErrorCode;
+    }
+    
+    std::vector<uint32_t> reorder(queries.size());
+    for(uint32_t i = 0; i < queries.size(); ++i) reorder[i] = i;
+    std::sort(reorder.begin(), reorder.end(), [&queries](auto a, auto b){
+
+        if(queries[a]->isReachability() != queries[b]->isReachability())
+            return queries[a]->isReachability() > queries[b]->isReachability();
+        if(queries[a]->isLoopSensitive() != queries[b]->isLoopSensitive())
+            return queries[a]->isLoopSensitive() < queries[b]->isLoopSensitive();
+        if(queries[a]->containsNext() != queries[b]->containsNext())
+            return queries[a]->containsNext() < queries[b]->containsNext();
+        return queries[a]->formulaSize() < queries[b]->formulaSize();
+    });
+
+    //Write queries to output file
+    writeQueries(queries, querynames, reorder, outputQueryFile, builder.getPlaceNames());
+
+    //Write net to output file
+    auto net = std::unique_ptr<PetriNet>(builder.makePetriNet());
+    net->toXML(outputModelFile, cpnBuilder.isTimed(), cpnBuilder.getInvariants(), options.outputVerifydtapnFormat);
+    
+    
+       
+    //return SuccessCode;
+}
 
