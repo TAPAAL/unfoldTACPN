fa8400a47578137ad8133e2aa831219af41a1310
Peter G. Jensen <root@petergjoel.dk>
2022-01-04 14:44:47 +0100

    objects

--- include/PQL/Visitor.h
+++ include/PQL/Visitor.h
@@ -46,35 +46,20 @@ namespace unfoldtacpn
 
             // Quantifiers, most uses of the visitor will not use the quantifiers - so we give a default implementation.
             // default behaviour is error
-            virtual void _accept(const ControlCondition*)
-            {   assert(false); std::cerr << "No accept for ControlCondition" << std::endl; exit(0);};
-            virtual void _accept(const EFCondition*)
-            {   assert(false); std::cerr << "No accept for EFCondition" << std::endl; exit(0);};
-            virtual void _accept(const EGCondition*)
-            {   assert(false); std::cerr << "No accept for EGCondition" << std::endl; exit(0);};
-            virtual void _accept(const AGCondition*)
-            {   assert(false); std::cerr << "No accept for AGCondition" << std::endl; exit(0);};
-            virtual void _accept(const AFCondition*)
-            {   assert(false); std::cerr << "No accept for AFCondition" << std::endl; exit(0);};
-            virtual void _accept(const EXCondition*)
-            {   assert(false); std::cerr << "No accept for EXCondition" << std::endl; exit(0);};
-            virtual void _accept(const AXCondition*)
-            {   assert(false); std::cerr << "No accept for AXCondition" << std::endl; exit(0);};
-            virtual void _accept(const EUCondition*)
-            {   assert(false); std::cerr << "No accept for EUCondition" << std::endl; exit(0);};
-            virtual void _accept(const AUCondition*)
-            {   assert(false); std::cerr << "No accept for AUCondition" << std::endl; exit(0);};
+            virtual void _accept(const ControlCondition*);
+            virtual void _accept(const EFCondition*);
+            virtual void _accept(const EGCondition*);
+            virtual void _accept(const AGCondition*);
+            virtual void _accept(const AFCondition*);
+            virtual void _accept(const EXCondition*);
+            virtual void _accept(const AXCondition*);
+            virtual void _accept(const EUCondition*);
+            virtual void _accept(const AUCondition*);
 
             // shallow elements, neither of these should exist in a compiled expression
-            virtual void _accept(const KSafeCondition* element)
-            {   assert(false); std::cerr << "No accept for KSafeCondition" << std::endl; exit(0);};
-            virtual void _accept(const BooleanCondition* element)
-            {   assert(false); std::cerr << "No accept for BooleanCondition" << std::endl; exit(0);};
-            virtual void _accept(const ShallowCondition *element) {
-                assert(false);
-                std::cerr << "No accept for ShallowCondition" << std::endl;
-                exit(0);
-            }
+            virtual void _accept(const KSafeCondition* element);
+            virtual void _accept(const BooleanCondition* element);
+            virtual void _accept(const ShallowCondition *element);
 
 
             // Expression
@@ -86,8 +71,7 @@ namespace unfoldtacpn
             virtual void _accept(const SubtractExpr* element) = 0;
 
             // shallow expression, default to error
-            virtual void _accept(const IdentifierExpr* element)
-            {   assert(false); std::cerr << "No accept for IdentifierExpr" << std::endl; exit(0);};
+            virtual void _accept(const IdentifierExpr* element);
         };
     }
 }
